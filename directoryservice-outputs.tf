92666236e0bac0a2a266b5c1f9afa44f27189fe0
infinitus0 <jacobs@cinegy.com>
2021-06-28 17:52:16 +0100

    -comment out ssm doc output

--- directoryservice-outputs.tf
+++ directoryservice-outputs.tf
@@ -2,7 +2,7 @@ output "directory_service" {
   value = aws_directory_service_directory.ad
 }
 
-output "ad_join_doc" {
-  value = aws_ssm_document.ad_join_doc
-}
+#output "ad_join_doc" {
+#  value = aws_ssm_document.ad_join_doc
+#}
 
