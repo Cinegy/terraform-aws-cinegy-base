10d77f23e59500c7553e23a9ad1ef1d7af7d4743
Lewis Kirkaldie <lewis@cinegy.com>
2021-06-30 17:58:14 +0100

    -fix indexing in conditional DS

--- directoryservice.tf
+++ directoryservice.tf
@@ -23,39 +23,39 @@ resource "aws_directory_service_directory" "ad" {
   }
 }
 
-#resource "aws_ssm_document" "ad_join_doc" {
-#  name          = "ad_join_${var.app_name}-${var.environment_name}"
-#  document_type = "Command"
-#  count    = var.ad_enabled ? 1 : 0
-#
-#  content = <<DOC
-#    {
-#            "schemaVersion": "1.0",
-#            "description": "Join an instance to a domain",
-#            "runtimeConfig": {
-#            "aws:domainJoin": {
-#                "properties": {
-#                    "directoryId": "${aws_directory_service_directory.ad[index].id}",
-#                    "directoryName": "${var.domain_name}",
-#                    "dnsIpAddresses": [
-#                        "${sort(aws_directory_service_directory.ad[index].dns_ip_addresses).0}",
-#                        "${sort(aws_directory_service_directory.ad[index].dns_ip_addresses).1}"                    
-#                    ]
-#                }
-#            }
-#            }
-#    }
-#    
-#DOC
-#
-#
-#  tags = {
-#    Env = var.environment_name
-#    App = var.app_name
-#    CUSTOMER  = var.customer_tag
-#    Terraform = true
-#  }
-#
-#  depends_on = [aws_directory_service_directory.ad]
-#}
+resource "aws_ssm_document" "ad_join_doc" {
+  name          = "ad_join_${var.app_name}-${var.environment_name}"
+  document_type = "Command"
+  count    = var.ad_enabled ? 1 : 0
+
+  content = <<DOC
+    {
+            "schemaVersion": "1.0",
+            "description": "Join an instance to a domain",
+            "runtimeConfig": {
+            "aws:domainJoin": {
+                "properties": {
+                    "directoryId": "${aws_directory_service_directory.ad[count.index].id}",
+                    "directoryName": "${var.domain_name}",
+                    "dnsIpAddresses": [
+                        "${sort(aws_directory_service_directory.ad[count.index].dns_ip_addresses).0}",
+                        "${sort(aws_directory_service_directory.ad[count.index].dns_ip_addresses).1}"                    
+                    ]
+                }
+            }
+            }
+    }
+    
+DOC
+
+
+  tags = {
+    Env = var.environment_name
+    App = var.app_name
+    CUSTOMER  = var.customer_tag
+    Terraform = true
+  }
+
+  depends_on = [aws_directory_service_directory.ad]
+}
 
