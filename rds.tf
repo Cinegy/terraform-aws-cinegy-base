85ebd08e1a1512bb1cfc90fd45a961b540312e88
infinitus0 <jacobs@cinegy.com>
2021-07-14 14:41:40 +0100

    -test to remove whitespace from policy

--- rds.tf
+++ rds.tf
@@ -194,29 +194,29 @@ EOF
  {
    "Version": "2012-10-17",
     "Statement": [
-         {
-             "Effect": "Allow",
-             "Action": [
-                 "s3:ListBucket",
-                 "s3:GetBucketLocation"
-             ],
-             "Resource": [
-                 "arn:aws:s3:::*"
-             ]
-         },
-         {
-             "Effect": "Allow",
-             "Action": [
-                 "s3:GetObject",
-                 "s3:PutObject",
-                 "s3:ListMultipartUploadParts",
-                 "s3:AbortMultipartUpload"
-             ],
-             "Resource": [
-                 "arn:aws:s3:::*/*"
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+          ],
+          "Resource": [
+            "arn:aws:s3:::${local.s3_rdsbackup_bucket}"
             ]
-         }
-      ]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListMultipartUploadParts",
+          "s3:AbortMultipartUpload"
+          ],
+          "Resource": [
+            "arn:aws:s3:::${local.s3_rdsbackup_bucket}/*"
+            ]
+      }
+    ]
  } 
  EOF
 
