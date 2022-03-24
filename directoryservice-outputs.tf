c7b661f8c1926fcb2a43c2c91b932a8d46aa6042
infinitus0 <jacobs@cinegy.com>
2022-03-24 12:34:21 +0000

    -Add AD doc output from module

--- directoryservice-outputs.tf
+++ directoryservice-outputs.tf
@@ -2,7 +2,7 @@ output "directory_service" {
   value = aws_directory_service_directory.ad
 }
 
-#output "ad_join_doc" {
-#  value = aws_ssm_document.ad_join_doc
-#}
+output "ad_join_doc" {
+ value = element(concat(aws_ssm_document.ad_join_doc, tolist([""])), 0)
+}
 
