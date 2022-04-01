565c2898cc223519a8661a6b7787d828b846e95f
infinitus0 <jacobs@cinegy.com>
2022-04-01 14:56:57 +0100

    -Move to acl resouce, terraform depreciation warn

--- buckets.tf
+++ buckets.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "infra" {
   bucket = "${lower(var.app_name)}-infra-${var.environment_name}"
-  acl    = "private"
 
 
    tags = {
@@ -15,4 +14,9 @@ resource "aws_s3_bucket" "infra" {
   lifecycle {
     ignore_changes = [tags]
   }
+}
+
+resource "aws_s3_bucket_acl" "infra_acl" {
+  bucket = aws_s3_bucket.infra.id
+  acl    = "private"
 }
